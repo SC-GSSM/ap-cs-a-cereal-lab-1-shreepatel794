50909591df448e57237b55a9fc17490d11634823
shreepatel794 <shreepatel794@gmail.com>
2026-03-22 16:01:58 +0000

    chungus

--- src/main/java/Cereal.java
+++ src/main/java/Cereal.java
@@ -1,21 +1,47 @@
 /**
  * Cereal.java
  *
- * @author – Your name
- * @author – Class period
+ * @author – Shree Patel
+ * @author – MWF - 8am 
  *
  */
 public class Cereal
 {
-    // private instance variables to hold info about a cereal from the data set
-    // the name, calories, fiber, carbohydrates, and cups
-
+    //private4 instant variables
+    private String name;
+    private int calories;
+    private double fiber;
+    private double carbs;
+    private int cups;
+    
     // constructor that creates a cereal object
+    public Cereal(String name, int calories, double fiber, double carbs, int cups){
+    this.name = name;
+    this.calories = calories;
+    this.fiber = fiber;
+    this.carbs = carbs;
+    this.cups = cups;
+}
 
     // accessor methods for each instance variable
+    public String getName(){return this.name;}
+    public int getCal(){return this.calories;}
+    public double getFiber(){return this.fiber;}
+    public double getCarbs(){return this.carbs;}
+    public int getCups(){return this.cups;}
+
+    public void setName(String n){this.name = n;}
+    public void setCal(int calo){this.calories = calo;}
+    public void setFiber(double f){this.fiber = f;}
+    public void setCarbs(double carb){this.carbs = carb;}
+    public void setCups(int cup){this.cups = cup;}
 
     /* toString method returns the values stored in the instance variables
        concatenated in a sentence starting with "Cereal: " */
+   public String toString(){
+    return "Cereal: " + name + " Calories: " +calories +" Fiber: " + fiber + " Carbs: " + carbs + "Cups: " + cups;
+   }
+    
 
 }
 
--- src/main/java/CerealRunner.java
+++ src/main/java/CerealRunner.java
@@ -6,29 +6,40 @@ import java.io.FileReader;
 
 public class CerealRunner
 {
+    private ArrayList<Cereal> cereals;
     // create a private instance variable to store an ArrayList of Cereal objects
 
     public CerealRunner(String fileName)
     {
+        cereals = new ArrayList<Cereal>();
         // instantiate the ArrayList
 
         try
         {
             FileReader fileRdr = new FileReader(fileName);
             Scanner scan = new Scanner(fileRdr);
+            scan.nextLine();
             while(scan.hasNext())
             {
                 String myStr = scan.nextLine();
 
-                // use the split method to parse the data into an array of
+                 // use the split method to parse the data into an array of
                 //   String objects
-
+                String[] data = myStr.split(",");
+               
                 // go through each String array element and save it
                 //   into the appropriate variable which will be used to
                 //   create a Cereal object
+                String name = data[0]; 
+                
+                int calories = Integer.parseInt(data[1]);
+              
+                double sugar = Double.parseDouble(data[2]);
+               
 
                 // create a new Cereal object, and add it to the ArrayList
-
+                 Cereal c = new Cereal(name, calories, sugar);
+                 cereals.add(c);
             }
             scan.close();
 
